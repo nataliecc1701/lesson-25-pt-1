52cb1f7786376f6ebc894be9792ec84b67b6f1ce
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-01 15:14:34 -0800

    added validators

--- forms.py
+++ forms.py
@@ -2,12 +2,15 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Optional, NumberRange, URL, AnyOf
+
+allowed_species = ["Cat", "Dog", "Porcupine"]
 
 class AddPetForm(FlaskForm):
     '''form to add pets'''
-    name = StringField("Name of Pet")
-    species = StringField("Species")
-    photo_url = StringField("Photo of pet (URL)")
-    age = IntegerField("Age (full years)")
+    name = StringField("Name of Pet", validators=[InputRequired()])
+    species = StringField("Species", validators=[AnyOf(allowed_species)])
+    photo_url = StringField("Photo of pet (URL)", validators=[Optional(), URL()])
+    age = IntegerField("Age (full years)", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes")
     
